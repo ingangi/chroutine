0d8eaac9416bc61b9faa77ce22c48151d9a7d88d
ingangi <huahui_c@hotmail.com>
2019-03-25 17:51:02 +0800

    example for how to check the rpc client connection state

--- examples/rpc_example/test_client/test_client_main.cpp
+++ examples/rpc_example/test_client/test_client_main.cpp
@@ -13,15 +13,27 @@ int main(int argc, char **argv)
             return;
         }
 
+        LOG("grpc_async_client_t %p created, is it ready? %d", client, client->ready());
+
         int i = 0;
         call_Test_HowAreYou caller(client);
         rpcpb::TestRsp rsp;
 
-        while (client) {
-            ::grpc::StatusCode code = caller.call_sync(rsp);
-            LOG << "call_Test_HowAreYou code=" << code << ", rsp:" << rsp.rsp() << std::endl;
+        while (client) {            
+            // you can wait for the client connected before do the call.
+            // or you can just call it without check the ready state, then the StatusCode will return 14 on connection not ready.
+            // if (!client->ready()) {
+            //     LOG << "waiting for client tobe ready" << std::endl;
+            //     SLEEP(20);
+            //     continue;
+            // }
 
-            if (++i >= 3) {
+            LOG("try to call call_Test_HowAreYou the %dth time, is it ready? %d", i, client->ready());
+            ::grpc::StatusCode code = caller.call_sync(rsp);
+            LOG << "call_Test_HowAreYou code=" << code << ", rsp:" << rsp.rsp() << ". clinet ready? " << client->ready() << std::endl;
+
+            // how we destroy a client
+            if (++i >= 10) {
                 LOG << "grpc_async_client_t will unregister now.\n";
                 client->unregister_from_engin();
                 client = nullptr;
